1f4646a5f82b62971a05d01e014bf09849768959
duongbd <blackdiamond6129@gmail.com>
2021-07-02 10:20:00 +0700

    default practice 1

---
+++ src/practice_1/Main.java
@@ -0,0 +1,16 @@
+package practice_1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        try {
+            int number = scanner.nextInt();
+            String string= (number>=0) ?"This is positive integers":"This is negative integers";
+            System.out.println(string);
+        }catch (Exception e){
+            System.out.println("It's not an integers");
+        }
+    }
+}
