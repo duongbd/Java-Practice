c0b853aaeb8fcb8f7e10acf28c99e75329ae80e3
duongbd <blackdiamond6129@gmail.com>
2021-07-03 08:56:21 +0700

    practice 5

---
+++ src/practice_5/Main.java
@@ -0,0 +1,16 @@
+package practice_5;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            String idStudentl = scanner.next("B[\\d]{7}");
+            System.out.println(idStudentl + " is match");
+        } catch (InputMismatchException e) {
+            System.out.println("Not match");
+        }
+    }
+}
