d4284b9cfeb98306559e4b0ef5c3a6326a4934d0
duongbd <blackdiamond6129@gmail.com>
2021-07-03 09:27:05 +0700

    practice 7

---
+++ src/practice_7/Main.java
@@ -0,0 +1,18 @@
+package practice_7;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int number = scanner.nextInt();
+            for (int i=1;i<=20;i++){
+                System.out.println(number+" x "+i+" = "+number*i);
+            }
+        } catch (InputMismatchException exception) {
+            System.out.println("It's not an integers");
+        }
+    }
+}
