fea1aa399935eddc377a6e99e5826c2ea47b8667
duongbd <blackdiamond6129@gmail.com>
2021-07-03 09:11:54 +0700

    practice 6

---
+++ src/practice_6/Main.java
@@ -0,0 +1,23 @@
+package practice_6;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int number = scanner.nextInt();
+            if (number > 0) {
+                int sum = 0;
+                for (int i = 0; i <= number; i += 2) {
+                    sum += i;
+                }
+                System.out.println("Sum= " + sum);
+            } else
+                System.out.println("Integer must >=0");
+        } catch (InputMismatchException exception) {
+            System.out.println("It's not an integers");
+        }
+    }
+}
