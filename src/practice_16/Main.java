e50a490a6a90afffe6b2f92246d4630e9a71aba1
duongbd <blackdiamond6129@gmail.com>
2021-07-05 11:15:53 +0700

    practice 16

---
+++ src/practice_16/Main.java
@@ -0,0 +1,17 @@
+package practice_16;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string=scanner.next();
+        String subString=scanner.next();
+        if (subString.length()==1){
+            if (string.contains(subString)){
+                System.out.println("Yes");
+            }else System.out.println("No");
+        }
+        else System.out.println("Second input must a char");
+    }
+}
