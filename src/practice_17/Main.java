d0c8c3c360f5eeca21f93b201c96845132051f69
duongbd <blackdiamond6129@gmail.com>
2021-07-05 11:21:20 +0700

    practice 17

--- src/practice_17/Main.java
+++ src/practice_17/Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         String string = scanner.next();
         String subString = scanner.next();
